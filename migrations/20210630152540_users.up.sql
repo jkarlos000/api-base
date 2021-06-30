0b21476abd0c85c4ede9e4122a826ef3e5c8e00e
Juan Carlos Ulloa Vasquez <juancarlosulloavasquez@MacBook-Pro-de-Juan.local>
2021-06-30 12:46:48 -0400

    Chage migration create

--- migrations/20210630152540_users.up.sql
+++ migrations/20210630152540_users.up.sql
@@ -5,9 +5,9 @@ CREATE TABLE users
     password    VARCHAR(150) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    is_active INT NOT NULL,
-    first_name  VARCHAR(50) NOT NULL,
-    last_name   VARCHAR(50) NOT NULL,
+    is_active INT default 1 NOT NULL,
+    first_name  VARCHAR(50) default 'Jhon' NOT NULL,
+    last_name   VARCHAR(50) default 'Doe' NOT NULL,
     constraint users_username_uindex
         unique (username)
 );
