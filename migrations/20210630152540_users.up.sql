56eeced8899258fbe4ee92a3f7505f23611b95e1
jkarlos000 <jc.ulloa@jkarlos.info>
2021-07-01 13:39:00 -0400

    Sw1 3 (#12)
    * sw1-3: user package service
    
    * sw1-3: fixed databases
    
    * sw1-3: fixed pull

--- migrations/20210630152540_users.up.sql
+++ migrations/20210630152540_users.up.sql
@@ -4,9 +4,9 @@ CREATE TABLE users
     username       VARCHAR(50) NOT NULL,
     email       VARCHAR(100) NOT NULL,
     password    VARCHAR(150) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    is_active INT default 1 NOT NULL,
+    created_at   TIMESTAMP NOT NULL,
+    updated_at   TIMESTAMP NOT NULL,
+    is_active    BOOLEAN DEFAULT TRUE NOT NULL,
     first_name  VARCHAR(50) default 'Jhon' NOT NULL,
     last_name   VARCHAR(50) default 'Doe' NOT NULL,
     constraint users_username_uindex
