4b8a64b26d88ac8b26b2f0b3a09400961ca1eb2f
jkarlos <jc.ulloa@jkarlos.info>
2021-07-01 11:05:30 -0400

    sw1-2: Fixed migrations

--- migrations/20210630145942_sessions.up.sql
+++ migrations/20210630145942_sessions.up.sql
@@ -3,15 +3,15 @@ CREATE TABLE sessions
     id         VARCHAR(36) PRIMARY KEY,
     owner       VARCHAR(36) NOT NULL,
     tittle      VARCHAR(100) DEFAULT 'Diagrama C4 Nivel 1' NOT NULL,
-    description VARCHAR(1024) DEFAULT 'Únete para diseñar el Sistema!' NOT NULL,
-    password    VARCHAR(100) DEFAULT '' NOT NULL ,
+    description VARCHAR(1024) DEFAULT 'Unete para diseñar el Sistema' NOT NULL,
+    password    VARCHAR(100) NOT NULL,
     url         VARCHAR(512) NOT NULL,
     slug        VARCHAR(36) NOT NULL ,
     data        VARCHAR(512) NOT NULL,
     is_active INT default 1 NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP,
     constraint sessions_slug_uindex
-        unique (slug),
+        unique (slug)
 );
