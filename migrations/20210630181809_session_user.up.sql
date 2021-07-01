232e050506b6eb1b8b883138c31f7cf659724697
jkarlos <jc.ulloa@jkarlos.info>
2021-07-01 02:06:25 -0400

    sw1-1: changin session_user to rooms

--- migrations/20210630181809_session_user.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE session_user
-(
-    session_id      VARCHAR (36) NOT NULL,
-    user_id         VARCHAR(36) NOT NULL,
-    role_id         VARCHAR(36) NOT NULL,
-    is_active       INT  DEFAULT 1  NOT NULL
-);
