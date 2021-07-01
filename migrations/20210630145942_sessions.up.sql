447c07bd53a7b31b89fad328ea212b0936072d82
jkarlos <jc.ulloa@jkarlos.info>
2021-07-01 01:54:14 -0400

    sw1-1: fix

--- migrations/20210630145942_sessions.up.sql
+++ migrations/20210630145942_sessions.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE sessions
 (
     id         VARCHAR(36) PRIMARY KEY,
     userid       VARCHAR(36) NOT NULL,
-    url         VARCHAR(512) default "" NOT NULL,
+    url         VARCHAR(512) NOT NULL,
     slug        VARCHAR(36) NOT NULL ,
     data        VARCHAR(512) NOT NULL
 );
