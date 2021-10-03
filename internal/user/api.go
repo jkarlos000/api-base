0ec2310034ebd8f10dccb40bc27e32515bdb8881
jkarlos000 <jc.ulloa@jkarlos.info>
2021-10-02 20:21:58 -0400

    sc-1: update Users (#17)
    * sc-1: update Users
    
    * sc-1: fix api
    
    * sc-1: fix repository

--- internal/user/api.go
+++ internal/user/api.go
@@ -1,28 +1,19 @@
 package user
 
 import (
-	"backend/internal/errors"
 	"backend/pkg/log"
 	"backend/pkg/pagination"
 	"encoding/json"
-	"net/http"
-
 	routing "github.com/go-ozzo/ozzo-routing/v2"
 )
 
 // RegisterHandlers sets up the routing of the HTTP handlers.
 func RegisterHandlers(r *routing.RouteGroup, service Service, authHandler routing.Handler, logger log.Logger) {
 	res := resource{service, logger}
-
-	r.Post("/users", res.create)
 	r.Use(authHandler)
-
 	// the following endpoints require a valid JWT
-	r.Get("/users/me", res.me)
 	r.Get("/users/<id>", res.get)
 	r.Get("/users", res.query)
-	// r.Put("/users/<id>", res.update)
-	// r.Delete("/users/<id>", res.delete)
 }
 
 type resource struct {
@@ -32,15 +23,6 @@ type resource struct {
 
 func (r resource) get(c *routing.Context) error {
 	user, err := r.service.Get(c.Request.Context(), c.Param("id"))
-	if err != nil {
-		return err
-	}
-
-	return c.Write(user)
-}
-
-func (r resource) me(c *routing.Context) error {
-	user, err := r.service.Me(c.Request.Context())
 	if err != nil {
 		return err
 	}
@@ -68,42 +50,3 @@ func (r resource) query(c *routing.Context) error {
 	pages.Items = users
 	return c.Write(pages)
 }
-
-
-func (r resource) create(c *routing.Context) error {
-	var input CreateUserRequest
-	if err := c.Read(&input); err != nil {
-		r.logger.With(c.Request.Context()).Info(err)
-		return errors.BadRequest("")
-	}
-	user, err := r.service.Create(c.Request.Context(), input)
-	if err != nil {
-		return err
-	}
-
-	return c.WriteWithStatus(user, http.StatusCreated)
-}
-
-// func (r resource) update(c *routing.Context) error {
-// 	var input UpdateUserRequest
-// 	if err := c.Read(&input); err != nil {
-// 		r.logger.With(c.Request.Context()).Info(err)
-// 		return errors.BadRequest("")
-// 	}
-
-// 	user, err := r.service.Update(c.Request.Context(), c.Param("id"), input)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return c.Write(user)
-// }
-
-// func (r resource) delete(c *routing.Context) error {
-// 	user, err := r.service.Delete(c.Request.Context(), c.Param("id"))
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return c.Write(user)
-// }
--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,7 +1,6 @@
 package user
 
 import (
-	"backend/internal/auth"
 	"backend/internal/entity"
 	"backend/internal/errors"
 	"backend/pkg/dbcontext"
@@ -14,8 +13,6 @@ import (
 
 // Repository encapsulates the logic to access users from the data source.
 type Repository interface {
-	// Me returns the user with the specified context.
-	Me(ctx context.Context) (entity.User, error)
 	// Get returns the user with the specified user ID.
 	Get(ctx context.Context, id string) (entity.User, error)
 	// Count returns the number of users.
@@ -24,10 +21,6 @@ type Repository interface {
 	Query(ctx context.Context, offset, limit int, term string, filters map[string]interface{}) ([]entity.User, error)
 	// Create saves a new user in the storage.
 	Create(ctx context.Context, user entity.User) error
-	// Update updates the user with given ID in the storage.
-	Update(ctx context.Context, user entity.User) error
-	// Delete removes the user with given ID from the storage.
-	// Delete(ctx context.Context, id string) error
 }
 
 // repository persists users in database
@@ -41,26 +34,6 @@ func NewRepository(db *dbcontext.DB, logger log.Logger) Repository {
 	return repository{db, logger}
 }
 
-// Me reads the user with the specified ID from the database.
-func (r repository) Me(ctx context.Context) (entity.User, error) {
-	identity := auth.CurrentUser(ctx)
-	var user entity.User
-
-	if err := r.db.With(ctx).Select("u.*").From("users as u").Where(dbx.HashExp{"u.id": identity.GetID()}).One(&user); err != nil {
-		return user, err
-	}
-
-	/*if err := r.db.With(ctx).Select("r.name as name").
-		From("roles as r").
-		LeftJoin("role_user as ru", dbx.NewExp("r.id = ru.role_id")).
-		Where(dbx.HashExp{"ru.user_id": user.ID}).
-		Column(&user.Roles); err != nil {
-		return user, err
-	}*/
-
-	return user, nil
-}
-
 // Get reads the user with the specified ID from the database.
 func (r repository) Get(ctx context.Context, id string) (entity.User, error) {
 	var user entity.User
@@ -68,37 +41,10 @@ func (r repository) Get(ctx context.Context, id string) (entity.User, error) {
 	return user, err
 }
 
-// Create saves a new user record in the database.
-// It returns the ID of the newly inserted user record.
-func (r repository) Create(ctx context.Context, user entity.User) error {
-	var count int
-
-	if err := r.db.With(ctx).Select("COUNT(*)").From("users").Where(dbx.HashExp{"username": user.Username}).Row(&count); err != nil {
-		return err
-	}
-
-	if count > 0 {
-		return errors.BadRequest("username already exists")
-	}
-
-	user.IsActive = true
-
-	return r.db.With(ctx).Model(&user).Insert()
-}
-
 // Update saves the changes to an user in the database.
 func (r repository) Update(ctx context.Context, user entity.User) error {
 	return r.db.With(ctx).Model(&user).Update()
 }
-
-// Delete deletes an user with the specified ID from the database.
-// func (r repository) Delete(ctx context.Context, id string) error {
-// 	user, err := r.Get(ctx, id)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return r.db.With(ctx).Model(&user).Delete()
-// }
 
 // Count returns the number of the user records in the database.
 func (r repository) Count(ctx context.Context) (int, error) {
@@ -133,4 +79,21 @@ func (r repository) Query(ctx context.Context, offset, limit int, term string, f
 		Limit(int64(limit)).
 		All(&users)
 	return users, err
+}
+// Create saves a new user record in the database.
+// It returns the ID of the newly inserted user record.
+func (r repository) Create(ctx context.Context, user entity.User) error {
+	var count int
+
+	if err := r.db.With(ctx).Select("COUNT(*)").From("users").Where(dbx.HashExp{"username": user.Username}).Row(&count); err != nil {
+		return err
+	}
+
+	if count > 0 {
+		return errors.BadRequest("username already exists")
+	}
+
+	user.IsActive = true
+
+	return r.db.With(ctx).Model(&user).Insert()
 }
